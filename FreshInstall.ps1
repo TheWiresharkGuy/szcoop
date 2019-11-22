52dddbc67181e60f03e06b602bb00ab19d8bb58a
lksz <lksz@lksz.me>
2019-11-21 21:52:38 -0500

    Update alias setup
    - refactor: the way alias removal is done, now it will be done right before addition.
    - refactor: cleanup unnecessary call to start-process, instead used *>&1 | Out-Null for clean redirect of output.
    - added: alias-list alias.
    - added: update information (if exists) in export-ps

--- FreshInstall.ps1
+++ FreshInstall.ps1
@@ -45,27 +45,19 @@ scoop bucket add extras
 # Add scoop-completion bucket, followed by installation
 scoop bucket add scoop-completion https://github.com/liuzijing97/scoop-completion
 scoop install scoop-completion
-$local:aliasList = scoop | Out-String -Width 17; # instead of scoop alias list which outputs to out-warning when empty
-'autocomplete-off','autocomplete-on','export-ps','refresh' | Where-Object { $aliasList -match $_ } | ForEach-Object { scoop alias rm $_ |  out-null }
-scoop alias add autocomplete-on  'Get-Module -ListAvailable $env:SCOOP\modules\* | Where-Object Name -eq scoop-completion | Import-Module'
-scoop alias add autocomplete-off 'Get-Module scoop-completion | Remove-Module'
-# add a scoop alias, to create a collection of objects:
-scoop alias add export-ps 'param([string[]]$filter,[switch]$WithPath)
- scoop export | sls ''^(.+)\W\(v:(.+)\)( \*global\*)? \[(.+)\]$'' |% {
-     $local:p=$_.matches.groups;
-     if($filter -and $p[1].value -notin $filter){return};
-     $local:r = [ordered]@{
-         AppName=$p[1].value;
-         Version=$p[2].value;
-         Scope=if($p[3].value){"Global"}else{"Local"};
-         Bucket=$p[4].value
-     };
-     if($WithPath){
-         $r.AppPath=$(($local:tmp = scoop info $p[1].value) | sls "Installed:" | % { Join-Path (Split-Path -Parent (($tmp[$_.LineNumber].Replace(" *global*","")).Trim())) "current" })
-     };
-     [PSCustomObject]($r)
- }'
-scoop alias add refresh "Write-Host -Foreground DarkGreen 'Refreshing scoop...'; Start-Process -NoNewWindow -Wait cmd -ArgumentList '/c','scoop','update','>NUL'; scoop status"
+$local:installAliases = [ordered]@{}
+$installAliases['autocomplete-on' ] = 'Get-Module -ListAvailable $env:SCOOP\modules\* | Where-Object Name -eq scoop-completion | Import-Module'
+$installAliases['autocomplete-off'] = 'Get-Module scoop-completion | Remove-Module'
+# alias to create an objects based export:
+$installAliases['export-ps'       ] = 'param([string[]]$filter,[switch]$WithPath) $local:update = [ordered]@{}; $local:in = ""; $a | % { $local:line = $_; switch (($line -split " ")[0]){ "Updates" { $in = "update" } "" { if( $in -ne "" ) { $local:u = [ordered]@{}; $_u = ($line -split ":"); $u.Name = $_u[0].Trim(); $u.Current, $u.Available = ($_u[1].Trim() -split " -> "); Invoke-Expression "`$$in[''$($u.Name)''] = [PSCustomObject]`$u"; } } default {$in = ""} } }; scoop export | sls "^(.+)\W\(v:(.+)\)( \*global\*)? \[(.+)\]$" |% { $local:p=$_.matches.groups; if($filter -and $p[1].value -notin $filter){return}; $local:r = [ordered]@{ AppName=$p[1].value; Version=$p[2].value; Scope=if($p[3].value){"Global"}else{"Local"}; Bucket=$p[4].value }; if( $update.Count -gt 1 ) { $r.UpdateAvailable = $update.Values | Where-Object Name -eq $r.AppName | Select-Object -ExpandProperty Available }; if($WithPath){ $r.AppPath=$(($local:tmp = scoop info $p[1].value) | sls "Installed:" | % { Join-Path (Split-Path -Parent (($tmp[$_.LineNumber].Replace(" *global*","")).Trim())) "current" }) }; [PSCustomObject]($r) }'
+$installAliases['alias-list'      ] = 'scoop alias list *>&1 | Out-String -Stream | sls "^[^ ]+" | % { $_.matches.Value }'
+# alias, for cleaner output of refresh:
+$installAliases['refresh'         ] = 'Write-Host -Foreground DarkGreen "Refreshing scoop..."; (scoop update *>&1 | Out-Null); scoop status'
+
+# add aliases defined above, making sure it is overwritten by any other alias already existing.
+$installAliases.Keys | % { $(scoop alias rm $_ *>&1 | out-null); scoop alias add $_ $installAliases[$_] }
+
+# activate autocomplete (with alias defined above)
 scoop autocomplete-on
 
 ## prepare PowerShell default profile:
